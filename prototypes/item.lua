2f7eb53a4f6329efcf5da28268aeae1570de276e
Matthew Vance <32961154+vipervire@users.noreply.github.com>
2019-09-24 22:12:18 -0500

    Fixed some typos

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -4,7 +4,6 @@ data:extend{
 		name = "advanced-electric-furnace",
 		icon = "__base__/graphics/icons/electric-furnace.png",
 		icon_size = 32,
-		flags = {"goes-to-quickbar"},
 		subgroup = "smelting-machine",
 		order = "d[advanced-electric-furnace]",
 		place_result = "advanced-electric-furnace",
